3ec32e3d3290e3a6272d4ac2d5cae1f8dcb9668b
Anudeep Rentala <anudeep.rentala@gmail.com>
2017-01-14 13:02:01 -0800

    added usage

--- parallel.js
+++ parallel.js
@@ -20,3 +20,24 @@ executeAsync = function (funcs, callback) {
             result[i] = data;
         });
     }
+
+    //usage
+
+    function func1(cb) {
+        setTimeout(function () {
+            cb("func1");
+        },300)
+    }
+    function func2(cb) {
+        setTimeout(function () {
+            cb("func2");
+        },200)
+    }
+    function func3(cb) {
+        cb({ data : "data", foo: "foo"})
+    }
+    executeAsync([func1, func2, func3], function (res) {
+        console.log(res);
+        res[0];  //result of func1
+        res[1];  //result of func2
+    });
